5e27954700441a177cc0f49cca9d4e0e318f5f66
K9Design <dog@k9design.co.uk>
2021-11-17 15:37:50 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,43 +1,53 @@
-import './App.css';
-import React, { useState } from 'react';
-import ChuckNorrisCard from './Components/ChuckCard';
-import ChuckJokes from './Components/ChuckJokes';
-import ChuckInfo from './Components/ChuckInfo';
+import "./App.css";
+import React, { useState } from "react";
+import ChuckNorrisCard from "./Components/ChuckCard";
+import ChuckJokes from "./Components/ChuckJokes";
+import ChuckInfo from "./Components/ChuckInfo";
 
 function App() {
+  const [chuckGreeting, setChuckGreeting] = useState("I am Chuck Norris!!!");
+  const [chuckImage, setChuckImage] = useState(
+    "https://images-global.nhst.tech/image/R1dwYnJhOTc5RjNCTG5URi9BMUlIZGE5cTRtemFrUWFBSVNINUMvZ20xZz0=/nhst/binary/f1ffde963b9087457feb601be1842d16?image_version=1200"
+  );
+  const [whalesSaved, setWhalesSaved] = useState(700);
+  const [roundHouseKicks, setRoundHouseKicks] = useState(300000);
+  const [jokes, setJokes] = useState([
+    {
+      id: 1,
+      joke: "Chuck Norris doesn’t read books. He stares them down until he gets the information he wants.",
+    },
+    {
+      id: 2,
+      joke: "Time waits for no man. Unless that man is Chuck Norris.",
+    },
+    {
+      id: 3,
+      joke: "The dinosaurs looked at Chuck Norris the wrong way once. You know what happened to them.",
+    },
+    {
+      id: 4,
+      joke: "Chuck Norris does not own a stove, oven, or microwave, because revenge is a dish best served cold.",
+    },
+  ]);
 
-  const [chuckGreeting, setChuckGreeting] = useState("I am Chuck Norris!!!")
-  const [chuckImage, setChuckImage] = useState("https://images-global.nhst.tech/image/R1dwYnJhOTc5RjNCTG5URi9BMUlIZGE5cTRtemFrUWFBSVNINUMvZ20xZz0=/nhst/binary/f1ffde963b9087457feb601be1842d16?image_version=1200")
-  const [whalesSaved, setWhalesSaved] = useState(700)
-  const [roundHouseKicks, setRoundHouseKicks] = useState(300000)
-  const [jokes, setJokes] = useState([{
-    "id": 1,
-    "joke": "Chuck Norris doesn’t read books. He stares them down until he gets the information he wants.",
-  },
-  {
-    "id": 2,
-    "joke": "Time waits for no man. Unless that man is Chuck Norris.",
-  },
-  {
-    "id": 3,
-    "joke": "The dinosaurs looked at Chuck Norris the wrong way once. You know what happened to them.",
-  },
-  {
-    "id": 4,
-    "joke": "Chuck Norris does not own a stove, oven, or microwave, because revenge is a dish best served cold.",
-  }])
+  let joke = jokes.filter(({ id }) => id === 3);
+  console.log(joke)
 
   return (
     <div className="App">
-
       <h1>React props and state</h1>
-      <ChuckNorrisCard />
+      <ChuckNorrisCard chuckGreeting={chuckGreeting} chuckImage={chuckImage} />
 
       <h2>Chuck Info: </h2>
-      <ChuckInfo />
+      <ChuckInfo whalesSaved={whalesSaved} roundHouseKicks={roundHouseKicks} />
 
       <h2>Jokes: </h2>
+      {jokes.map(({ joke, id }) => (
+        <ChuckJokes key={id} joke={joke} />
+      ))}
 
+      <h2>Filtered: </h2>
+      {<ChuckJokes key={joke[0].id} joke={joke[0].joke} />}
     </div>
   );
 }
